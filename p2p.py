e1ffb6c43ddd8e03f94ec61c36a03d0de359c20a
aallali <contact@allali.me>
2025-01-23 13:18:16 +0100

    fix: set server to always listen to  port `12345`

--- p2p.py
+++ p2p.py
@@ -216,6 +216,7 @@ def main():
     host, port = read_config()
 
     if args.server:
+        port = 12345
         server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server.bind(("0.0.0.0", port))
